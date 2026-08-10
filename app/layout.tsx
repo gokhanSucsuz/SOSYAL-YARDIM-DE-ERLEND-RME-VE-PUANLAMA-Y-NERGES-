a69c84bd3b873f34a4ded970504d834f209ea6f8
Gokhan Sucsuz <gokhansucsuz@gmail.com>
2026-08-10 12:33:10 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,8 @@ import type {Metadata} from 'next';
 import './globals.css'; // Global styles
 
 export const metadata: Metadata = {
-  title: 'My Google AI Studio App',
-  description: 'My Google AI Studio App',
+  title: 'Sosyal Yardım İnceleme ve Nesnel Değerlendirme Sistemi',
+  description: 'Sosyal Yardım İnceleme ve Nesnel Değerlendirme Sistemi (SYD-NDS) Sosyal Adalet, Hakkaniyetli Kaynak Dağıtımı ve Dijital Dönüşüm Projesi',
 };
 
 export default function RootLayout({children}: {children: React.ReactNode}) {
